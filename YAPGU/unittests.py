ce198d29142e688e72a4ecd8bd26d43e6bd40fba
szx <simonrgk@gmail.com>
2013-01-17 20:10:47 +0100

    Added unit test for microthread module.

--- YAPGU/unittests.py
+++ YAPGU/unittests.py
@@ -26,7 +26,8 @@ def failedTaunt():
     return "failed (%s)!" % random.choice(failedTaunts)
 
 
-from Vector import Vector2D
+import mathf
+from mathf import Vector2D
 def vectorMathTest():
     assert Vector2D(1.0,1.0) + Vector2D(2.0,3.0) == Vector2D(3.0,4.0)
     assert Vector2D(1.0,4.0) - Vector2D(2.0,3.0) == Vector2D(-1.0,1.0)
@@ -42,10 +43,39 @@ def vectorMathTest():
     v /= 2.0
     assert v == Vector2D(1.5,1.5)
     assert -v == -Vector2D(1.5,1.5)
-    assert str(v) == "(1.6,1.5)", "__repr__test"
+    assert str(v) == "(1.5,1.5)", "__repr__test"
 vectorMathTest.text = "Vector math (boring)..."
 
-tests = [vectorMathTest]
+import microthreads
+from microthreads import wait, schedule, microthread, run, count
+def microthreadsTest():
+    buff = [] # We will not use stdout.
+    class Pinger(object): 
+        def __init__(self, amount, message = "PING"):
+            self.amount = amount # Loop counter.
+            self.message = message
+            microthread(self.update)
+        def update(self):
+            while self.amount > 0:
+                buff.append(self.message)
+                self.amount -= 1
+                schedule()
+    p1 = Pinger(10)
+    assert count() == 1
+    run()
+    schedule() #Schedule microthreads.
+    assert len(buff) == 10, "One microthreaded 'pinger' test - length of buffer is %s, not expected 10." % len(buff) 
+    buff = []
+    p1 = Pinger(10)
+    p2 = Pinger(10, "PONG")
+    assert count() == 2
+    run()
+    schedule() #Schedule microthreads.
+    assert len(buff) == 20, "Two microthreads 'pinger' test - length of buffer is %s, not expected 20." % len(buff) 
+
+microthreadsTest.text = "Microthreads (more boring)..."
+
+tests = [vectorMathTest, microthreadsTest]
 
 def unitTest():
     print "Self-done unit tests! (hurray...)"
