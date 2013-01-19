558ca9531118bc521d9620baf8ac63570e7aa441
szx <simonrgk@gmail.com>
2013-01-19 21:04:10 +0100

    Added some dumb keyboard input example.

--- YAPGU/main.py
+++ YAPGU/main.py
@@ -3,8 +3,9 @@ Main module.
 Main file of our program. Pills here!
 """
 
-from microthreads import microthread, run
+import microthreads
 import graphics
+import inputf
 import logic
 from mathf import Vector2D
    
@@ -14,5 +15,16 @@ def start(argv):
     def drawFunc():
         sprite.draw()
     graphics.init(800, 600, drawFunc)
+    print graphics._window
+    
+    inputf.init()
+    def movementMicrothread():
+        while True:
+            if inputf.keyboard[inputf.key.UP]:
+                sprite.position.y += 10 * logic.delta()
+            microthreads.schedule()
+            
+    microthreads.microthread(movementMicrothread)
+    
     logic.start()
     
