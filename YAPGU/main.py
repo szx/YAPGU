bb34106f3f7bf6dbd7af8343d7bf6da679d1f7cb
szx <simonrgk@gmail.com>
2013-01-19 14:27:22 +0100

    New sprite sheet.

--- YAPGU/main.py
+++ YAPGU/main.py
@@ -10,7 +10,7 @@ from mathf import Vector2D
    
 def start(argv):
     graphics.assetDirectory('assets')
-    sprite = graphics.Sprite("enemy.jpg", Vector2D(100.0,100.0), frames=3)
+    sprite = graphics.Sprite("sprite.png", Vector2D(100.0,100.0), frames=9)
     def drawFunc():
         sprite.draw()
     graphics.init(800, 600, drawFunc)
