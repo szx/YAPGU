2f1d8d91d150623d13e0da668ea46dc797fa37a8
szx <simonrgk@gmail.com>
2013-01-19 13:33:33 +0100

    Example application!

--- YAPGU/main.py
+++ YAPGU/main.py
@@ -1,7 +1,18 @@
 """
 Main module.
-Main file of our program. Your game here.
+Main file of our program. Pills here!
 """
 
+from microthreads import microthread, run
+import graphics
+import logic
+from mathf import Vector2D
+   
 def start(argv):
-    print "Code of your marvelous game here!"
+    graphics.assetDirectory('assets')
+    sprite = graphics.Sprite("enemy.jpg", Vector2D(100.0,100.0), frames=3)
+    def drawFunc():
+        sprite.draw()
+    graphics.init(800, 600, drawFunc)
+    logic.start()
+    
